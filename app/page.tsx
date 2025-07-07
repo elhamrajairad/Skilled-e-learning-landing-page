55a076a12aca3ecfdbc58bb1b31048927c973d89
elham rajaee <elham.rajairad@gmail.com>
2025-07-07 20:24:19 +0330

    add section hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
-    <section className='container max-w-[1100px] mx-auto px-6 xl:px-0'>
+    <section className='container max-w-[1100px] mx-auto px-6 sm:px-15 xl:px-0 xs:overflow-hidden lg:overflow-visible  xs:relative '>
       <Header />
-      <main className='flex flex-col gap-8'>
-        <section className='hero'>
-
-        </section>
+      <main className='flex flex-col gap-8 '>
+        <Hero />
       </main>
     </section>
   )
