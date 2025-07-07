1614d84b6e3472c166d2f71d610f982148e27149
elham rajaee <elham.rajairad@gmail.com>
2025-07-07 18:29:39 +0330

    create component button

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,10 +1,11 @@
 import logo from "@/public/logo-dark.svg"
 import Image from "next/image"
+import Button from "./Button"
 export default function Header() {
     return (
-        <header className="flex justify-between items-center py-8">
+        <header className="flex justify-between items-center py-8 ">
             <Image src={logo} alt="logo" />
-            <button className="capitalize bg-[#13183f] text-white hover:bg-[#666ca3] cursor-pointer rounded-full px-6 py-3 font-bold">get started</button>
+            <Button variant="primaryGradient" size="sm" />
         </header>
     )
 }
